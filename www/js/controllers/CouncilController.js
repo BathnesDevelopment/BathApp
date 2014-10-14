fc02ac5904b8484a5a250b597ede8aa200e90ad7
Alex Bryson <alex@ransomti.me>
2014-10-14 10:18:22 +0100

    Moved data manipulation from to BathData
    For 2 way data binding

--- www/js/controllers/CouncilController.js
+++ www/js/controllers/CouncilController.js
@@ -1,116 +1,14 @@
 angular.module('MyBath.CouncilController', [])
-.controller('CouncilController', function ($scope, $ionicSideMenuDelegate) {
+.controller('CouncilController', function ($scope, $ionicSideMenuDelegate, BathData) {
     //$scope.reload = function () {
     //$scope.reloadCouncilData();
     //$scope.$broadcast('scroll.refreshComplete');
     //};
 
-    //$scope.reloadCouncilData = function () {
-    var i = 0;
-    var string = '';
-    var doc;
-    if ($scope.bathdata[13]) {
-        $scope.yourCouncillors = $scope.bathdata[13];
-    }
-    if ($scope.bathdata[14] && $scope.bathdata[14].Results.Listed_Building.Info !== "<p>No records found nearby.</p>") {
-        $scope.listedBuilding = $scope.bathdata[14];
-    }
-    if ($scope.bathdata[18]) {
-        $scope.councilOffices = $scope.bathdata[18];
-    }
-    if ($scope.bathdata[19]) {
-        $scope.housingAllowanceZones = $scope.bathdata[19];
-    }
+    $scope.yourCouncillors = BathData.get(13);
+    $scope.listedBuilding = BathData.get(14);
+    $scope.councilOffices = BathData.get(18);
+    $scope.housingAllowanceZones = BathData.get(19);
+    $scope.binCollection = BathData.get(7);
 
-    if ($scope.yourCouncillors && $scope.yourCouncillors.Results) {
-        var phoneNumbers = /(\+[0-9]{1,2}|0)[0-9 ]{7,12}/;
-        string = '<!DOCTYPE html><html><head></head><body>' + $scope.yourCouncillors.Results.Your_Councillors._ + '</body></html>';
-        doc = new DOMParser().parseFromString(string, 'text/html');
-
-        var councillorList = doc.querySelectorAll('div#myCouncillor a');
-        var councillors = []; // Some councillors have more than one URL returned from ishare
-        for (i = 0; i < councillorList.length; i++) {
-            if (councillorList[i].toString().search("democracy.bathnes") != -1) {
-                councillors.push(councillorList[i]);
-            }
-        }
-        var councillorInfo = doc.querySelectorAll('div#myCouncillor');
-        var tel = "";
-        var party = "";
-        if (councillors[0]) {
-
-            if (councillorInfo[0] && councillorInfo[0].innerText.indexOf('Telephone') != -1) {
-                tel = phoneNumbers.exec(councillorInfo[0].innerText)[0];
-            }
-            $scope.yourCouncillors.Results.Your_Councillors.number1 = councillors[0].innerHTML;
-            $scope.yourCouncillors.Results.Your_Councillors.info1 = councillors[0].href;
-            $scope.yourCouncillors.Results.Your_Councillors.telephone1 = tel;
-            $scope.yourCouncillors.Results.Your_Councillors.img1 = URLtoBase64(councillorInfo[0].getElementsByTagName('img')[0].src);
-        }
-        if (councillors[1]) {
-            tel = "";
-            party = "";
-
-            if (councillorInfo[1] && councillorInfo[1].innerText.indexOf('Telephone') != -1) {
-                tel = phoneNumbers.exec(councillorInfo[1].innerText)[0];
-            }
-            $scope.yourCouncillors.Results.Your_Councillors.number2 = councillors[1].innerHTML;
-            $scope.yourCouncillors.Results.Your_Councillors.info2 = councillors[1].href;
-            $scope.yourCouncillors.Results.Your_Councillors.telephone2 = tel;
-            $scope.yourCouncillors.Results.Your_Councillors.img2 = URLtoBase64(councillorInfo[1].getElementsByTagName('img')[0].src);
-        }
-        if (councillors[2]) {
-            tel = "";
-            party = "";
-
-            if (councillorInfo[2] && councillorInfo[2].innerText.indexOf('Telephone') != -1) {
-                tel = phoneNumbers.exec(councillorInfo[2].innerText)[0];
-            }
-            $scope.yourCouncillors.Results.Your_Councillors.number3 = councillors[3].innerHTML;
-            $scope.yourCouncillors.Results.Your_Councillors.info3 = councillors[3].href;
-            $scope.yourCouncillors.Results.Your_Councillors.telephone3 = tel;
-            $scope.yourCouncillors.Results.Your_Councillors.img3 = URLtoBase64(councillorInfo[1].getElementsByTagName('img')[0].src);
-        }
-    }
-
-    if ($scope.councilOffices && $scope.councilOffices.Results) {
-        // if iShare returns just one, then this fails. Re-jig the object to be a new object holding the old one
-        // Example: BA3 3UD
-        if (typeof $scope.councilOffices.Results.____________________________.length === typeof undefined) {
-            var res = $scope.councilOffices.Results.____________________________;
-            $scope.councilOffices.Results.____________________________ = {};
-            $scope.councilOffices.Results.____________________________[0] = res;
-            $scope.councilOffices.Results.____________________________.length = 1; // for iteration below
-        }
-        for (i = 0; i < $scope.councilOffices.Results.____________________________.length; i++) {
-            $scope.councilOffices.Results.____________________________[i].title = $scope.councilOffices.Results.____________________________[i].Your_nearest_Council_Office_is_.split('|')[1].replace('amp;', '');
-            $scope.councilOffices.Results.____________________________[i].url = $scope.councilOffices.Results.____________________________[i].Your_nearest_Council_Office_is_.split('|')[0].replace('amp;', '');
-            var geo = NEtoLL($scope.councilOffices.Results.____________________________[i].MapSpurE, $scope.councilOffices.Results.____________________________[i].MapSpurN);
-            $scope.councilOffices.Results.____________________________[i].lat = geo.latitude;
-            $scope.councilOffices.Results.____________________________[i].lon = geo.longitude;
-        }
-    }
-
-    if ($scope.bathdata[7]) {
-        $scope.binCollection = $scope.bathdata[7];
-    }
-
-    if ($scope.binCollection && $scope.binCollection.Results && $scope.binCollection.Results._______________) {
-        string = '<!DOCTYPE html><html><head></head><body>' + $scope.binCollection.Results._______________._ + '</body></html>';
-        doc = new DOMParser().parseFromString(string, 'text/html');
-        var collectionDates = doc.querySelectorAll('span.WasteHighlight');
-
-        if (collectionDates && collectionDates[0]) {
-            $scope.binCollection.Results._______________.waste = collectionDates[0].innerText;
-        }
-        if (collectionDates && collectionDates[1]) {
-            $scope.binCollection.Results._______________.recycling = collectionDates[1].innerText;
-        }
-        if (collectionDates && collectionDates[2]) {
-            $scope.binCollection.Results._______________.garden = collectionDates[2].innerText;
-        }
-    }
-    //};
-
-    //$scope.reloadCouncilData();
 });
--- www/js/services/BathData.js
+++ www/js/services/BathData.js
@@ -108,7 +108,7 @@ angular.module('MyBath.BathDataService', [])
                 angular.forEach(results, function (result) {
                     aggregatedData = aggregatedData.concat(result.data);
                 });
-                window.localStorage['BathData'] = angular.toJson(aggregatedData);
+                window.localStorage.BathData = angular.toJson(aggregatedData);
                 return aggregatedData;
             }, function (error) {
                 return [];
@@ -116,6 +116,112 @@ angular.module('MyBath.BathDataService', [])
         },
         clear: function () {
             window.localStorage.removeItem('BathData');
+        },
+        get: function ( id ) {
+            var i = 0;
+            var res = angular.fromJson(window.localStorage.BathData)[id];
+            var doc;
+            switch (id) {
+                // My council
+                case 7 :  // Collection dates
+                    if (res && res.Results && res.Results._______________) {
+                        string = '<!DOCTYPE html><html><head></head><body>' + res.Results._______________._ + '</body></html>';
+                        doc = new DOMParser().parseFromString(string, 'text/html');
+                        var collectionDates = doc.querySelectorAll('span.WasteHighlight');
+
+                        if (collectionDates && collectionDates[0]) {
+                            res.Results._______________.waste = collectionDates[0].innerText;
+                        }
+                        if (collectionDates && collectionDates[1]) {
+                            res.Results._______________.recycling = collectionDates[1].innerText;
+                        }
+                        if (collectionDates && collectionDates[2]) {
+                            res.Results._______________.garden = collectionDates[2].innerText;
+                        }
+                    }
+                    return res;
+                case 13:  // Councillor 
+                    var yourCouncillors = res;
+                    if (res && res.Results) {
+                        var phoneNumbers = /(\+[0-9]{1,2}|0)[0-9 ]{7,12}/;
+                        string = '<!DOCTYPE html><html><head></head><body>' + yourCouncillors.Results.Your_Councillors._ + '</body></html>';
+                        doc = new DOMParser().parseFromString(string, 'text/html');
+
+                        var councillorList = doc.querySelectorAll('div#myCouncillor a');
+                        var councillors = []; // Some councillors have more than one URL returned from ishare
+                        for (i = 0; i < councillorList.length; i++) {
+                            if (councillorList[i].toString().search("democracy.bathnes") != -1) {
+                                councillors.push(councillorList[i]);
+                            }
+                        }
+                        var councillorInfo = doc.querySelectorAll('div#myCouncillor');
+                        var tel = "";
+                        var party = "";
+                        if (councillors[0]) {
+
+                            if (councillorInfo[0] && councillorInfo[0].innerText.indexOf('Telephone') != -1) {
+                                tel = phoneNumbers.exec(councillorInfo[0].innerText)[0];
+                            }
+                            yourCouncillors.Results.Your_Councillors.number1 = councillors[0].innerHTML;
+                            yourCouncillors.Results.Your_Councillors.info1 = councillors[0].href;
+                            yourCouncillors.Results.Your_Councillors.telephone1 = tel;
+                            yourCouncillors.Results.Your_Councillors.img1 = URLtoBase64(councillorInfo[0].getElementsByTagName('img')[0].src);
+                        }
+                        if (councillors[1]) {
+                            tel = "";
+                            party = "";
+
+                            if (councillorInfo[1] && councillorInfo[1].innerText.indexOf('Telephone') != -1) {
+                                tel = phoneNumbers.exec(councillorInfo[1].innerText)[0];
+                            }
+                            yourCouncillors.Results.Your_Councillors.number2 = councillors[1].innerHTML;
+                            yourCouncillors.Results.Your_Councillors.info2 = councillors[1].href;
+                            yourCouncillors.Results.Your_Councillors.telephone2 = tel;
+                            yourCouncillors.Results.Your_Councillors.img2 = URLtoBase64(councillorInfo[1].getElementsByTagName('img')[0].src);
+                        }
+                        if (councillors[2]) {
+                            tel = "";
+                            party = "";
+
+                            if (councillorInfo[2] && councillorInfo[2].innerText.indexOf('Telephone') != -1) {
+                                tel = phoneNumbers.exec(councillorInfo[2].innerText)[0];
+                            }
+                            yourCouncillors.Results.Your_Councillors.number3 = councillors[3].innerHTML;
+                            yourCouncillors.Results.Your_Councillors.info3 = councillors[3].href;
+                            yourCouncillors.Results.Your_Councillors.telephone3 = tel;
+                            yourCouncillors.Results.Your_Councillors.img3 = URLtoBase64(councillorInfo[1].getElementsByTagName('img')[0].src);
+                        }
+                    }
+                    return yourCouncillors;
+                case 14:  // Listed Building
+                    if ( res.Results.Listed_Building.Info === "<p>No records found nearby.</p>" ) {
+                        return {};
+                    }
+                    return res;
+                case 18:  // Council offices
+                    if (res && res.Results) {
+                        // if iShare returns just one, then this fails. Re-jig the object to be a new object holding the old one
+                        // Example: BA3 3UD
+                        if (typeof res.Results.____________________________.length === typeof undefined) {
+                            var toAppend = res.Results.____________________________;
+                            res.Results.____________________________ = {};
+                            res.Results.____________________________[0] = toAppend;
+                            res.Results.____________________________.length = 1; // for iteration below
+                        }
+                        for (i = 0; i < res.Results.____________________________.length; i++) {
+                            res.Results.____________________________[i].title = res.Results.____________________________[i].Your_nearest_Council_Office_is_.split('|')[1].replace('amp;', '');
+                            res.Results.____________________________[i].url = res.Results.____________________________[i].Your_nearest_Council_Office_is_.split('|')[0].replace('amp;', '');
+                            var geo = NEtoLL(res.Results.____________________________[i].MapSpurE, res.Results.____________________________[i].MapSpurN);
+                            res.Results.____________________________[i].lat = geo.latitude;
+                            res.Results.____________________________[i].lon = geo.longitude;
+                        }
+                    }
+                    return res;
+
+                default:
+                    return res;
+            }
+                
         }
     };
 });
