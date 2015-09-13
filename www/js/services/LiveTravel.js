e66461e5c09a95a371b307dbb7122555537843b7
Dave Rowe <david_rowe@bathnes.gov.uk>
2015-09-13 23:00:57 +0100

    Adding live travel service

---
+++ www/js/services/LiveTravel.js
@@ -0,0 +1,51 @@
+angular.module('MyBath.LiveTravelService', [])
+/**
+ * Factory: Live Travel
+*/
+.factory('LiveTravel', function ($http, $q, DataTransformations, config) {
+    return {
+        // Method: LiveTravel.all()
+        // Input: JSON[] / Empty
+        // Output: JSON / Empty Array
+        // Gets all the data back from local storage.
+        all: function () {
+            var liveTravel = window.localStorage.LiveTravel;
+            if (liveTravel) {
+                return angular.fromJson(liveTravel);
+            }
+            return [];
+        },
+        // Method: LiveTravel.save()
+        // Input: JSON
+        // Output: None
+        // Saves the data from a JSON input, back into the local storage (overwriting previous storage)
+        save: function (liveTravel) {
+            window.localStorage.LiveTravel = angular.toJson(liveTravel);
+        },
+        // Method: LiveTravel.fetchAll()
+        // Output: 
+        // Calls the live travel data service and returns the result.
+        fetchAll: function () {
+            var liveTravel = [];
+            var liveTravel_q = $q.defer();
+            $http.get(config.bathDataWS + "/GetLiveTravel")
+                .success(function (data, status, headers, config) {
+                    liveTravel = JSON.parse(data.GetLiveTravelResult);
+                    if (liveTravel && liveTravel != []) {
+                    }
+                    else {
+                        liveTravel = "Failed";
+                    }
+                    liveTravel_q.resolve(liveTravel);
+                })
+                .error(function (data, status, headers, config) {
+                    liveTravel = "Failed";
+                    liveTravel_q.resolve(bathData);
+                });
+            return liveTravel_q.promise;
+        },
+        clear: function () {
+            window.localStorage.removeItem('LiveTravel');
+        }
+    };
+});
