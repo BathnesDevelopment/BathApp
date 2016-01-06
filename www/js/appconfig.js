f0f0cbb9feaec09f34d4ebc41a6cd40bbf05532d
Dave <david_rowe@bathnes.gov.uk>
2016-01-06 16:44:47 +0000

    Changing web service links

--- www/js/appconfig.js
+++ www/js/appconfig.js
@@ -2,7 +2,7 @@
    .constant('config', {
        'bathDataWS': 'http://isharemapstest.bathnes.gov.uk/residentsapp/BathData.svc/GetAllData',
        'addressWS': 'http://isharemapstest.bathnes.gov.uk/residentsapp/BathData.svc/GetAddress',
-       'reportsWS': 'http://isharemapstest.bathnes.gov.uk/residentsapp/Open311ReportWS.svc',
+       'reportsWS': 'https://isharemapstest.bathnes.gov.uk/residentsapp/Open311ReportWS.svc',
        'planningWS': 'http://isharemapstest.bathnes.gov.uk/residentsapp/PlanningComments.svc',
        'mapLayerConfigWS': 'http://isharemapstest.bathnes.gov.uk/residentsapp/BathData.svc/GetAvailableMapLayers',
        'mapLayerWS': 'http://isharemapstest.bathnes.gov.uk/residentsapp/BathData.svc/GetMapLayer/',
